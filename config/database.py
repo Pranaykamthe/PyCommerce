c481f744a1e5b30e0a28b6f9734e74a8bf50aa49
Pranaykamthe <pranaykamthe512@gmail.com>
2026-08-10 13:18:36 +0530

    Remove hardcoded database password fallback

--- config/database.py
+++ config/database.py
@@ -20,7 +20,7 @@ load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Luffy")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME", "pycommerce")
 
 
